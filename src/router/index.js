c5989c49ec6e2104604666bf47faf4da3bdb135f
allanmsilva23 <allanmartinssilva23@gmail.com>
2025-05-26 14:45:24 -0300

    linha dplicada createWebHistory removida

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
-import { createWebHistory } from 'vue-router';
 
 const router = createRouter({
   history: createWebHistory('/vital-reciclagem-frontend/'),
