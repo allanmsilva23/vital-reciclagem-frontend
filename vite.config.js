7070f72a6f432666f244ede6915a9b53379e96a5
allanmsilva23 <allanmartinssilva23@gmail.com>
2025-05-26 14:33:40 -0300

    Configura deploy automático

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import path from 'path'
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'path'; 
 
 export default defineConfig({
-  base: '/vital-reciclagem-frontend/',
   plugins: [vue()],
+  base: '/vital-reciclagem-frontend/', 
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src')
-    }
-  }
-})
+      '@': path.resolve(__dirname, './src'), 
+    },
+  },
+});
